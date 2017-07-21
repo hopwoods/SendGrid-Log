11d7e07e17f54175c1efe59229a49bce3a2dc7f3
Stuart Hopwood <stuart.hopwood@interactivets.com>
2017-07-21 17:35:48 +0100

    Getting JSON Working

--- SendGrid Log/Controllers/HomeController.cs
+++ SendGrid Log/Controllers/HomeController.cs
@@ -45,81 +45,24 @@ namespace SendGrid_Log.Controllers
         }
 
         // GET: Home/LogEvent
-        public IActionResult LogEvent()
-        {
-            
-            return View();
-        }
+        //public IActionResult LogEvent()
+        //{
+        //    return View();
+        //}
 
         // POST: Home/LogEvent
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult LogEvent(EmailEvent emailEvent)
+        [HttpGet]
+        public JsonResult LogEvent(Person person)
         {
+            //var parser = new WebhookParser();
+            //var events = await parser.ParseWebhookEventsAsync(Request.Body).ConfigureAwait(false);
 
-            if (ModelState.IsValid)
-            {
-                var parser = new WebhookParser();
-                var events = parser.ParseWebhookEvents(Request.Body.ToString());
+            return Json(person);
 
-                _context.Add(emailEvent);
-                _context.SaveChangesAsync();
-                return Ok();
-            }
-            return Ok();
-        }
 
-        // GET: Home/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var emailEvent = await _context.EmailEvent.SingleOrDefaultAsync(m => m.ID == id);
-            if (emailEvent == null)
-            {
-                return NotFound();
-            }
-            return View(emailEvent);
-        }
-
-        // POST: Home/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Response,Status,EventID,MessageID,EventType,EmailAddress,Timestamp,SmtpID,UniqueArgKey,Categories,Newsletter,ASMGroupID,Reason,Type,IP,TLS,Cert_Err,UserAgent,URL,URLOffset,Attempt,MarketingCampaignID,MarketingCampaignName,MarketingCampaignVersion,MarketingCampaignSplitID,PostType")] EmailEvent emailEvent)
-        {
-            if (id != emailEvent.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(emailEvent);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmailEventExists(emailEvent.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            return View(emailEvent);
         }
 
         // GET: Home/Delete/5
